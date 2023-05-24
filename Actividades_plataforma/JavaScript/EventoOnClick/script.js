5913d918921b21a372ecfdabc7dcaa390183eef9
GabiOyarzo <gabioyarzoe@gmail.com>
2023-05-24 12:55:28 -0400

    main

--- Actividades_plataforma/JavaScript/EventoOnClick/script.js
+++ Actividades_plataforma/JavaScript/EventoOnClick/script.js
@@ -1,3 +1,9 @@
+// Javascript de Actividad  OnClick   
+// Gabriela Oyarzo Escudero
+// fecha: 23/05/2023
+// última actualización: 24/05/2023
+
+
 function LogInOut(element) {
     element.innerText = "Logout";
 }
@@ -5,3 +11,17 @@ function LogInOut(element) {
 function hide(element) {
     element.remove();
 }
+
+let contador1 = 5;
+function AddCont1(element){
+    contador1 ++;
+    element.innerText = ( contador1 + " likes");
+    alert("Ninja was liked");
+};
+
+let contador2 = 17;
+function AddCont2(element){
+    contador2 ++;
+    element.innerText = ( contador2 + " likes");
+    alert("Ninja was liked");
+};
