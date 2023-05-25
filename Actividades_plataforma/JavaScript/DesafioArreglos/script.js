024c66e21365ff214b4b95584f6e3f6cec93b72e
GabiOyarzo <gabioyarzoe@gmail.com>
2023-05-25 13:49:49 -0400

    main

---
+++ Actividades_plataforma/JavaScript/DesafioArreglos/script.js
@@ -0,0 +1,38 @@
+// Desafio arreglos
+// Gabriela Oyarzo
+// Fecha: 25 de mayo de 2023
+// --------------------------------------------------------------------------------------------
+// 1.- Siempre hambriento
+// --------------------------------------------------------------------------------------------
+//function alwaysHungry(arr) {
+//    var x = []; 
+//    for(var aux1 = 0; arr.length > aux1; aux1++){
+//        if(arr[aux1] == "comida"){
+//          x.push("delicioso");
+//        }
+//    }
+//    if(x.length > 0){
+//        console.log(x)
+//    } else{
+//        console.log("Tengo Hambre")
+//    }
+// }
+// alwaysHungry([3.14, "comida", "pastel", true, "comida"]);
+// alwaysHungry([4, 1, 5, 7, 2]);
+// --------------------------------------------------------------------------------------------
+// 2.- Filtro paso alto
+// --------------------------------------------------------------------------------------------
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    // tu código aquí
+    return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // esperamos de vuelta [6, 8, 10, 9]
+
+
+// 3.- Mejor que el promedio
+
+// 4.- Arreglo invertido
+
+// 5.- Arreglo de Fibonacci
