cc0cf6f32feac8229774ef34d18d9822efc8c04c
GabiOyarzo <gabioyarzoe@gmail.com>
2023-06-06 11:54:03 -0400

    main

--- Actividades_plataforma/JavaScript/Calculadora/script.js
+++ Actividades_plataforma/JavaScript/Calculadora/script.js
@@ -3,19 +3,45 @@
 // Gabriela Oyarzo
 // 29 de mayo de 2023
 // ----------------------------------------------
-var display = document.getElementById("#display");
-var numero = [];
+var display = document.getElementById("display");
+var num1 = '';
+var num2 = '';
+var operador = '';
 
 function press(num) {
-    numero.push(num);
-    display.innerText = numero;
+    num2 += num;
+    display.innerText = num2;
 }
 
-function setOP(op){
-
+function setOP(op) {
+    display.innerText = op;
+    operador = op;
+    num1 = num2;
+    num2 = '';
+    
+}
+function clr() {
+    num2.substring = '';
 }
 
-function calculate(){
-
+function calculate() {
+    var resultado;
+    switch (operador) {
+        case '+':
+            resultado = parseFloat(num1) + parseFloat(num2);
+            break;
+        case '-':
+            resultado = parseFloat(num1) - parseFloat(num2);
+            break;
+        case '*':
+            resultado = parseFloat(num1) * parseFloat(num2);
+            break;
+        case '/':
+            resultado = parseFloat(num1) / parseFloat(num2);
+            break;
+        default:
+            resultado = NaN; // Operador no válido
+    }
+    display.innerText = resultado;
+    
 }
-
