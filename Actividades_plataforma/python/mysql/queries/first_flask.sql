3adc13cddc7fbba17ed5666fb50f11837eaa87b9
GabiOyarzo <gabioyarzoe@gmail.com>
2023-07-06 21:03:30 -0400

    main

---
+++ Actividades_plataforma/python/mysql/queries/first_flask.sql
@@ -0,0 +1,5 @@
+# use first_flask;
+# insert into first_flask.friends(first_name, last_name, occupation, created_at, updated_at)
+# values ("Gabriela","Oyarzo","Oceanografa",now(),now()), ("Fernando","Oyarzo","Kinesiologo",now(),now());
+
+select * from first_flask.friends
